463a466cfe52902d4b259307d1a95c96ebc7ae76
Samuel Okpe <38431147+samokpe2@users.noreply.github.com>
2023-05-28 03:30:27 +0100

    learning react native

--- donewithit/App.js
+++ donewithit/App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,
+  TouchableWithoutFeedback, 
+  TouchableOpacity,
+  TouchableHighlight,
+   SafeAreaView, Image} from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => console.log("Text Clicked");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Hello React Native </Text>
+      {/* <Image style={styles.logo} source={require('./assets/icon.png')} /> */}
+      {/* <TouchableWithoutFeedback onPress={() => console.log("Image Tapped")}>
+      <Image style={styles.logo} 
+      blurRadius={10}
+      source={{
+        width: 200,
+        height:200,
+        uri: "https://picsum.photos/id/1/200/300"}} />
+      </TouchableWithoutFeedback>
+
+      <TouchableOpacity onPress={() => console.log("Image Tapped")}>
+      <Image style={styles.logo} 
+      source={{
+        width: 200,
+        height:200,
+        uri: "https://picsum.photos/id/1/200/300"}} />
+      </TouchableOpacity>
+
+      <TouchableHighlight onPress={() => console.log("Image Tapped")}>
+      <Image style={styles.logo} 
+      source={{
+        width: 200,
+        height:200,
+        uri: "https://picsum.photos/id/1/200/300"}} />
+      </TouchableHighlight> */}
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +46,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center',
+    alignItems:'center',
   },
+  // logo:{
+  //   width:00,
+  //   height:100
+  // }
 });
